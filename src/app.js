9bb59690ffefcd66b35544400cdb5b78ddd94df3
VarshaAdigaBV <varsha.adiga@bazaarvoice.com>
2022-04-11 17:18:47 +0530

    Approach 2

--- src/app.js
+++ src/app.js
@@ -19,11 +19,16 @@ app.post('/logger', (req, res) => {
     // analyticsRequest(req.body, req.headers)
     res.send({"Logged":"yes"})
 }) 
-//Approach 2
+//Approach 2(in-progress)
 app.post('/logger/url', (req,res)=> {
     var url = req.body.url;
     let params = (new URL(url)).searchParams;
-    console.log(params)
+    if(params.get('r_batch')){
+        var batch = params.get('r_batch').replace(/,/g, "&").replace(/:/g, "=");//setting r_batch in query param format to make later processing easier
+        params.set('r_batch',batch)
+        var batchUrl = params.get('r_batch').match(/\(([^()]*)\)/g).map(function($0) { return $0.substring(1,$0.length-1) })//extracting each event from batch
+        console.log( new URLSearchParams(batchUrl[0]) )
+    }
     res.send({"Logged":"yes"})
 })
 
